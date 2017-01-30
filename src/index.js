4282a4e50367bdc6e94fba32d9c5faa672ca3fc1
mikhail-riabokon <mikhail.riabokon@gmail.com>
2017-01-30 19:43:19 +0200

    init validator options

--- src/index.js
+++ src/index.js
@@ -1,7 +1,68 @@
 function throwError(error) {
-  throw new Erorr(error);
+  throw new Error(error);
 }
 
-function Validator(options) {}
+function message(message) {
+  console.log(message);
+}
+
+function getKeys(obj) {
+  var keys = [];
+
+  for (var key in obj) {
+    keys.push(key);
+  }
+
+  return keys;
+}
+
+function getNames(nodelist) {
+  var names = [];
+
+  for (var i = 0; i < nodelist.length; i++) {
+    var name = nodelist[i].getAttribute('name');
+
+    names.push(name);
+  }
+
+  return names;
+}
+
+function checkFieldsInForm(filedsNames, form) {
+  var formElementsNames = getNames(form.elements);
+  var formId = form.getAttribute('id');
+
+  for (var i = 0; i < filedsNames.length; i++) {
+    if (formElementsNames.indexOf(filedsNames[i]) === -1) {
+      message('There is no ' + filedsNames[i] + ' field in form #' + formId);
+    }
+  }
+}
+
+function Validator(options) {
+  if (options.formId) {
+    this.form = document.getElementById(options.formId);
+  } else {
+    throwError('Missed formId option');
+  }
+
+  if (options.validate && typeof options.validate === 'object') {
+    this.validate = options.validate;
+
+    var filedsNames = getKeys(options.validate);
+
+    if (filedsNames.length === 0) {
+      throwError('Validate fields can not be empty');
+    }
+
+    checkFieldsInForm(filedsNames, this.form);
+
+  } else {
+    throwError('Missed validate option');
+  }
+
+
+  return this;
+}
 
 window.Validator = Validator;
--- tests/index.tests.js
+++ tests/index.tests.js
@@ -2,4 +2,41 @@ describe('Validator', function () {
   it('should be defined', function () {
     expect(Validator).toBeDefined();
   });
+
+  it('should be a constructor function', function () {
+    expect(typeof Validator).toBe('function');
+  });
+
+  describe('init', function() {
+    beforeEach(function () {
+      var form = document.createElement('form');
+      var emailField = document.createElement('input');
+      var div = document.createElement('div');
+      var body = document.getElementsByTagName('body')[0];
+
+      emailField.setAttribute('name', 'email');
+      form.setAttribute('id', 'form');
+
+      div.appendChild(emailField);
+      form.appendChild(div);
+      body.appendChild(form);
+    });
+
+    it('options should be passed correctly', function () {
+      var correctValidateObject = {
+        email: {
+          validator: 'email',
+          message: 'Fail'
+        }
+      };
+      var validator = new Validator({
+        formId: 'form',
+        validate: correctValidateObject
+      });
+
+      expect(validator.form).not.toBe(null);
+      expect(validator.form).toBeDefined();
+      expect(validator.validate).toBeDefined()
+    });
+  });
 });
